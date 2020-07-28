adb697d1e6af41096ca1d78cb9385c5196d722f4
Tóth Máté <tothmate911@gmail.com>
2020-07-28 14:23:21 +0200

    something is wrong again

--- src/main/java/com/example/demo/DataInitializer.java
+++ src/main/java/com/example/demo/DataInitializer.java
@@ -7,6 +7,8 @@ import com.example.demo.repository.VehicleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
@@ -19,9 +21,12 @@ public class DataInitializer implements CommandLineRunner {
 
     private final UserRepository users;
 
+    private final PasswordEncoder passwordEncoder;
+
     public DataInitializer(VehicleRepository vehicles, UserRepository users) {
         this.vehicles = vehicles;
         this.users = users;
+        passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
     @Override
@@ -33,17 +38,17 @@ public class DataInitializer implements CommandLineRunner {
         vehicles.findAll().forEach(v -> log.debug(" Vehicle :" + v.toString()));
 
         users.save(VehicleAppUser.builder()
-            .username("user")
-            .password("password")
-            .roles(Arrays.asList("ROLE_USER"))
-            .build()
+                .username("user")
+                .password(passwordEncoder.encode("password"))
+                .roles(Arrays.asList("ROLE_USER"))
+                .build()
         );
 
         users.save(VehicleAppUser.builder()
-            .username("admin")
-            .password("password")
-            .roles(Arrays.asList("ROLE_USER", "ROLE_ADMIN"))
-            .build()
+                .username("admin")
+                .password(passwordEncoder.encode("password"))
+                .roles(Arrays.asList("ROLE_USER", "ROLE_ADMIN"))
+                .build()
         );
         log.debug("printing all users...");
         users.findAll().forEach(v -> log.debug(" VehicleAppUser :" + v.toString()));
